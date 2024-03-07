b3d470b330725efde364b143fb8226e1ebd15486
alanregisdesign <alanregisdesign@gmail.com>
2024-03-06 22:49:29 -0300

    versão RC1

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-    emailjs.init({
-        publicKey:'yybRw4DPg4xwDoLVN'
-    }); // Substitua com seu ID de usuário do EmailJS
+document.addEventListener('DOMContentLoaded', function() {
+    // Inicializa o EmailJS com a sua public key
+    emailjs.init('yybRw4DPg4xwDoLVN');
+  
+    // Configuração inicial para abrir o modal
+    var modal = document.getElementById("infoModal");
+    var btn = document.getElementById("send-email");
+    var span = document.getElementsByClassName("close-button")[0];
+    
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+    
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
 
     const services = [
         { name: 'Gestão de Tráfego', price: 600, description: 'Relatório com métricas e criação de estratégias para crescimento da rede social.', quantity: 1, mandatory: true },
@@ -56,35 +74,73 @@ document.addEventListener('DOMContentLoaded', () => {
     function updateTotalPrice() {
         document.getElementById('total-price').textContent = `Preço Total: R$ ${totalPrice.toFixed(2)}`;
     }
-
-    document.getElementById('send-email').addEventListener('click', function() {
-        let selectedServices = services
-        .map((service, index) => { // Mova o uso do index para cá
-            const isChecked = service.checkbox ? document.querySelector(`.include-service[data-index="${index}"]`).checked : true;
-            const quantity = document.querySelector(`.quantity[data-index="${index}"]`) ? document.querySelector(`.quantity[data-index="${index}"]`).value : 1;
-            if (service.mandatory || isChecked || quantity > 0) {
-                return `${service.name}: R$${service.price.toFixed(2)}${service.checkbox || !service.mandatory ? '' : ' x ' + quantity}`;
+  
+    // Adiciona o ouvinte de evento de submit para o formulário dentro do modal
+    document.getElementById('additional-info-form').addEventListener('submit', function(e) {
+      e.preventDefault();
+      // Coleta os valores do formulário
+      var clientName = document.getElementById('name').value;
+      var clientEmail = document.getElementById('email').value;
+      var company = document.getElementById('company').value;
+      var phone = document.getElementById('phone').value;
+  
+      // Prepara os serviços selecionados e a mensagem
+    let selectedServices = services
+        .map((service, index) => {
+            // Verifica se a checkbox está marcada ou se é um serviço obrigatório.
+            const isChecked = service.checkbox ? document.querySelector(`.include-service[data-index="${index}"]`).checked : false;
+            const quantityInput = document.querySelector(`.quantity[data-index="${index}"]`);
+            let quantity = 0;
+            
+            if (service.mandatory) {
+                quantity = 1; // Serviços obrigatórios contam como 1
+            } else if (isChecked) {
+                quantity = 1; // Checkbox marcada conta como 1
+            } else if (quantityInput) {
+                quantity = parseInt(quantityInput.value, 10); // Pega a quantidade do input se disponível
+            }
+            
+            if (quantity > 0) {
+                return `${service.name} (Quantidade: ${quantity}): R$${(service.price * quantity).toFixed(2)}`;
             }
             return '';
         })
-        .filter(item => item !== '') // Remove strings vazias
+        .filter(item => item !== '')
         .join('\n');
-      
-        let emailParams = {
-          email1: document.getElementById('email1').value,
-          email2: document.getElementById('email2').value,
-          itemList: selectedServices,
-          finalPrice: `R$ ${totalPrice.toFixed(2)}`
-        };
-      
-        emailjs.send('service_v2zyea8', 'template_srtp4fs', emailParams)
-          .then(function(response) {
-             console.log('SUCCESS!', response.status, response.text);
-             alert('Orçamento enviado com sucesso!');
-          }, function(error) {
-             console.log('FAILED...', error);
-             alert('Falha ao enviar o orçamento. Por favor, tente novamente.');
-        });
-    
+
+    let totalPrice = services.reduce((acc, service, index) => {
+        let quantity = 0;
+        if (service.mandatory) {
+            quantity = 1;
+        } else if (service.checkbox && document.querySelector(`.include-service[data-index="${index}"]`).checked) {
+            quantity = 1;
+        } else if (document.querySelector(`.quantity[data-index="${index}"]`)) {
+            quantity = parseInt(document.querySelector(`.quantity[data-index="${index}"]`).value, 10);
+        }
+        return acc + (quantity * service.price);
+    }, 0);
+  
+      // Objeto com os parâmetros para enviar no e-mail
+      let emailParams = {
+        to_email1: document.getElementById('email1').value,
+        // to_email2: document.getElementById('email2').value,
+        client_name: clientName,
+        client_email: clientEmail,
+        company: company,
+        phone: phone,
+        itemList: selectedServices,
+        finalPrice: `R$ ${totalPrice.toFixed(2)}`
+      };
+  
+      // Envia o e-mail usando EmailJS
+      emailjs.send('service_v2zyea8', 'template_srtp4fs', emailParams)
+        .then(function(response) {
+          console.log('SUCCESS!', response.status, response.text);
+          alert('Orçamento enviado com sucesso!');
+          modal.style.display = "none"; // Fecha o modal após o envio
+        }, function(error) {
+          console.log('FAILED...', error);
+          alert('Falha ao enviar o orçamento. Por favor, tente novamente.');
+      });
     });
-});
+  });
